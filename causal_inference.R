b267ecdf2569d00e9827ce7e900a7931d2b1a6fa
Samuel Padron <s.padronalcala@student.science.ru.nl>
2024-11-11 18:29:04 +0100

    Move dag definition to top

--- causal_inference.R
+++ causal_inference.R
@@ -6,80 +6,72 @@ library(dplyr)
 
 ESS11 <- read_csv("data/ESS11.csv")
 
+dag <- graphLayout(dagitty("dag{
+agea [exposure]
+alcfreq [outcome]
+cgtsmok [outcome]
+cntry [exposure]
+desire_to_seek_validation [latent]
+edulvlb 
+gndr [exposure]
+happy
+health [exposure]
+hinctnta [exposure]
+hlthhmp [outcome]
+impricha [outcome]
+innate_desire_to_help [latent]
+innate_desire_to_learn [latent]
+ipcrtiva [outcome]
+iphlppla [outcome]
+ipmodsta [outcome]
+iprspota [outcome]
+ipshabta [outcome]
+ipsucesa [outcome]
+ipudrsta [outcome]
+lrscale [outcome]
+slprl [outcome]
+stflife [outcome]
+agea -> edulvlb
+cntry -> edulvlb
+cntry -> happy
+cntry -> health
+cntry -> lrscale
+desire_to_seek_validation -> edulvlb
+desire_to_seek_validation -> impricha
+desire_to_seek_validation -> ipmodsta
+desire_to_seek_validation -> iprspota
+desire_to_seek_validation -> ipshabta
+desire_to_seek_validation -> ipsucesa
+desire_to_seek_validation -> stflife
+edulvlb -> alcfreq
+edulvlb -> cgtsmok
+edulvlb -> impricha
+edulvlb -> slprl
+edulvlb -> stflife
+gndr -> edulvlb
+happy -> ipsucesa
+health -> happy
+health -> hlthhmp
+hinctnta -> edulvlb
+innate_desire_to_help -> edulvlb
+innate_desire_to_help -> hlthhmp
+innate_desire_to_help -> iphlppla
+innate_desire_to_help -> ipudrsta
+innate_desire_to_help -> lrscale
+innate_desire_to_help -> stflife
+innate_desire_to_learn -> edulvlb
+innate_desire_to_learn -> impricha
+innate_desire_to_learn -> ipcrtiva
+innate_desire_to_learn -> ipsucesa
+innate_desire_to_learn -> stflife
+}"))
+
 # Which factors positively affect external validation-seeking?
 
 # How do factors such as age, gender, and socioeconomic status affect an individual’s educational attainment?
 #   Exposure variables: gndr, agea, hinctnta
 #   Outcome: edulvlb
 
-dag <- graphLayout(dagitty("dag {
-	innate_desire_to_learn[latent]
-	innate_desire_to_help[latent]
-	desire_to_seek_validation[latent]
-	agea[exposure]
-	cntry[exposure]
-	gndr[exposure]
-	health[exposure]
-	ipshabta[outcome]
-	ipmodsta[outcome]
-	ipsucesa[outcome]
-	iprspota[outcome]
-	impricha[outcome]
-	stflife[outcome]
-	ipcrtiva[outcome]
-	iphlppla[outcome]
-	hlthhmp[outcome]
-	ipudrsta[outcome]
-	lrscale[outcome]
-	alcfreq[outcome]
-	cgtsmok[outcome]
-	slprl[outcome]
-
-	agea     -> edulvlb
-	alcfreq  <- edulvlb
-	cgtsmok  <- edulvlb
-	cntry    -> { edulvlb happy lrscale health }
-	gndr     -> edulvlb
-	happy    -> ipsucesa
-	health   -> happy
-	health   -> hlthhmp
-
-	edulvlb  <- {
-		innate_desire_to_learn
-		innate_desire_to_help
-		desire_to_seek_validation
-	}
-
-	edulvlb -> {
-		slprl    
-		stflife
-		impricha
-	}
-
-	desire_to_seek_validation -> {
-		ipshabta
-		ipmodsta
-		ipsucesa
-		iprspota
-		impricha
-		stflife
-	}
-
-	innate_desire_to_learn -> {
-		impricha
-		ipcrtiva
-		ipsucesa
-		stflife
-	}
-
-	innate_desire_to_help -> {
-		iphlppla
-		hlthhmp  
-		ipudrsta
-		stflife
-		lrscale
-	}
-}"))
 
 plot(dag)
 
