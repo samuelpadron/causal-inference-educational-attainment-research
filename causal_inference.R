615e5786180f055d869a0058fd35541198f09b42
dmun <davidru01@live.nl>
2024-11-14 15:32:05 +0100

    Add data normalization

--- causal_inference.R
+++ causal_inference.R
@@ -29,46 +29,53 @@ d <- ESS11 %>%
     slprl <= 4,
     hlthhmp <= 3
   )
-d$edulvlb <- cut(d$edulvlb, breaks=c(0, 100, 200, 300, 400, 500, 600, 700, 800))
-d$edulvlb <- ordered(d$edulvlb)
-d$gndr <- as.integer(d$gndr)
+# d$edulvlb <- round(d$edulvlb / 100) * 100
+# d$edulvlb <- cut(d$edulvlb, breaks=c(0, 100, 200, 300, 400, 500, 600, 700, 800))
+# d$edulvlb <- ordered(d$edulvlb)
+# d$gndr <- as.integer(d$gndr)
 # d$agea <- as.integer(d$agea)
-d$agea <- scale(d$agea)
+# d$agea <- scale(d$agea)
 
-d <- subset(d, select=setdiff(names(dag), latents(dag)))
-d <- subset(d, select=-c(cntry))
+# d <- d %>% mutate(across(where(is.numeric), as.integer))
 
-dag_vars <- names(dag)
-d <- d %>% select(all_of(dag_vars))
-d <- d %>%
-  mutate(across(where(is.character), as.numeric)) %>%
-  mutate(across(where(is.factor), as.numeric))
+# d <- d %>% filter(!is.na(edulvlb))
+# d <- subset(d, select=setdiff(names(dag), latents(dag)))
+# d <- subset(d, select=-c(cntry))
 
-dag <- graphLayout(dagitty("dag{
+d <- d %>% mutate(across(where(is.numeric), scale))
+
+# dag_vars <- names(dag)
+# d <- d %>% select(all_of(dag_vars))
+# d <- d %>%
+#   mutate(across(where(is.character), as.numeric)) %>%
+#   mutate(across(where(is.factor), as.numeric))
+
+dag <- graphLayout(dagitty('
+dag {
 agea [exposure]
 alcfreq [outcome]
 cgtsmok [outcome]
 desire_to_seek_validation [latent]
-edulvlb 
 gndr [exposure]
-happy
 health [exposure]
 hinctnta [exposure]
 hlthhmp [outcome]
 impricha [outcome]
-innate_desire_to_help [latent]
-innate_desire_to_learn [latent]
-ipcrtiva [outcome]
-iphlppla [outcome]
 ipmodsta [outcome]
 iprspota [outcome]
 ipshabta [outcome]
 ipsucesa [outcome]
-ipudrsta [outcome]
 lrscale [outcome]
 slprl [outcome]
 stflife [outcome]
 agea -> edulvlb
+desire_to_seek_validation -> edulvlb
+desire_to_seek_validation -> impricha
+desire_to_seek_validation -> ipmodsta
+desire_to_seek_validation -> iprspota
+desire_to_seek_validation -> ipshabta
+desire_to_seek_validation -> ipsucesa
+desire_to_seek_validation -> stflife
 edulvlb -> alcfreq
 edulvlb -> cgtsmok
 edulvlb -> impricha
@@ -78,20 +85,40 @@ gndr -> edulvlb
 happy -> ipsucesa
 health -> happy
 health -> hlthhmp
-hinctnta -> edulvlb}"))
-
+hinctnta -> edulvlb
+}
+'))
 
 # Which factors positively affect external validation-seeking?
 
 # First step: Get factor values for the latent variables 'desire_to_seek_validation', 'innate_desire_to_help', 'innate_desire_to_learn'
 lvsem <- toString(dag, "lavaan")
-#cat(lvsem)
+cat(lvsem)
+lvsem.fit <- sem(lvsem, d)
+
+lvsem <- "
+edulvlb~agea
+desire_to_seek_validation=~edulvlb + impricha + ipmodsta + iprspota + ipshabta + ipsucesa + stflife
+alcfreq~edulvlb
+cgtsmok~edulvlb
+impricha~edulvlb
+slprl~edulvlb
+stflife~edulvlb
+edulvlb~gndr
+ipsucesa~happy
+happy~health
+hlthhmp~health
+edulvlb~hinctnta
+lrscale ~~ lrscale
+"
+cat(lvsem)
+lvsem.fit <- sem(lvsem, d)
 latent_variables <- 'desire_to_seek_validation =~ ipsucesa + ipshabta + ipmodsta + iprspota\ninnate_desire_to_help =~ ipudrsta + iphlppla + lrscale + hlthhmp\ninnate_desire_to_learn =~ ipsucesa + ipcrtiva + impricha'
 full_model <- paste(lvsem, latent_variables, sep="")
-#cat(full_model)
+cat(full_model)
 
 # Fit the CFA model and inspect latent variable covariance matrix (right now only using latent_variables, not complete DAG)
-fit <- cfa(latent_variables, data=d)
+fit <- cfa(lvsem, data=d)
 lavInspect(fit, "cov.lv")
 
 # Obtain latent factor scores and print them (these are the values we can use to add the three variables to the dataset)
